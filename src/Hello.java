b37698fa4fecd8a80d768d3a2b7e5e0bbacd4ed5
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-09-20 21:14:02 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Luke");
+        
+        int myFirstNumber = (10+5)*(20%6);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myLastOne);
+    }
+}
